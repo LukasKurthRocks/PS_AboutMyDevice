8e46152b6fc439bb3ff26835af53dc197816ec0a
Lukas Kurth Rocks <github@lukas.kurth.rocks>
2021-04-01 16:02:48 +0200

    RunPS1 Error Message

--- Sources/Run.ps1
+++ Sources/Run.ps1
@@ -1,2 +1,8 @@
-Set-Location "C:\ProgramData\GRT_AboutMyDevice"
-Start-Process -WindowStyle Hidden powershell.exe "C:\ProgramData\GRT_AboutMyDevice\About_this_computer.ps1"
+$ApplicationPath = "$env:ProgramData\GRT_AboutMyDevice"
+if (!(Test-Path -Path $ApplicationPath)) {
+    Write-Host "Path '$ApplicationPath' could not found!" -BackgroundColor Black -ForegroundColor Red
+    return 
+}
+
+Set-Location "$ApplicationPath"
+Start-Process -WindowStyle Hidden powershell.exe "$ApplicationPath\About_this_computer.ps1"
